ccc57a00ffb07ca884085453fe3c2d542979eb58
OmarZaatari <106358964+OmarZaatari@users.noreply.github.com>
2024-12-30 12:00:58 +0200

    Added user auth and account creation

--- app/dashboard/components/trend.js
+++ app/dashboard/components/trend.js
@@ -3,13 +3,11 @@ import { createClient } from "@/lib/supabase/server";
 
 export default async function Trend({ type, range }) {
   const supabase = createClient();
-  console.log("Hello");
   let { data, error } = await supabase.rpc("create_total", {
     range_arg: range,
     type_arg: type,
   });
   if (error) console.error(error);
-  console.log(data);
   const amounts = data[0];
   return (
     <BaseTrend
