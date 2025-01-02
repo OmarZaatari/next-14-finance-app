4cc10a0c734410324ffe4d2ccd322dba21a36d7a
OmarZaatari <106358964+OmarZaatari@users.noreply.github.com>
2025-01-02 16:55:11 +0200

    Change route.js

--- app/(auth)/auth/confirm/route.js
+++ app/(auth)/auth/confirm/route.js
@@ -1,26 +1,28 @@
-import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/server";
-export async function GET(request) {
+import { EmailOtpType } from '@supabase/supabase-js';
+import { NextRequest } from 'next/server';
+
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+
+export async function GET(NextRequest) {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get("token_hash");
-  const type = searchParams.get("type");
-  const next = searchParams.get("next") ?? "/";
-  const redirectTo = request.nextUrl.clone();
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete("token_hash");
-  redirectTo.searchParams.delete("type");
+  const token_hash = searchParams.get('token_hash');
+  const type = searchParams.get('type');
+  const next = searchParams.get('next') ?? '/';
+
   if (token_hash && type) {
-    const supabase = createClient();
+    const supabase = await createClient();
+
     const { error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
     });
     if (!error) {
-      redirectTo.searchParams.delete("next");
-      return NextResponse.redirect(redirectTo);
+      // redirect user to specified redirect URL or root of app
+      redirect(next);
     }
   }
-  // return the user to an error page with some instructions
-  redirectTo.pathname = "/error";
-  return NextResponse.redirect(redirectTo);
+
+  // redirect the user to an error page with some instructions
+  redirect('/login');
 }
